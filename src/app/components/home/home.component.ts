1c1d6c895c0b59ce765b1576e578029b7b8bed6b
Maciej Paluch <maciekpal@gmail.com>
2023-01-19 18:40:18 +0100

    Feat: Shop by category added to home page
    List of categories with images displayed on home page.
    After click on the card, user gets moved to "/category/:id" page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryModel } from '../../models/category.model';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +11,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  readonly CategoryList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+
+  constructor(private _categoriesService: CategoriesService) {}
 }
