6def4f10e692c4f340fccac8c591f7e5e9e1f5ec
Maciej Paluch <maciekpal@gmail.com>
2023-01-19 19:36:34 +0100

    Feat: Added store list to home page
    Displaying store list with name, tags and distance in km

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
+import { StoreQueryModel } from 'src/app/query-models/store.query-model';
 import { CategoryModel } from '../../models/category.model';
+import { StoreModel } from '../../models/store.model';
+import { TagModel } from '../../models/tag.model';
 import { CategoriesService } from '../../services/categories.service';
+import { StoresService } from '../../services/stores.service';
+import { TagsService } from '../../services/tags.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +16,27 @@ import { CategoriesService } from '../../services/categories.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  readonly CategoryList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly categoryList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly tagsMap$: Observable<Record<string, TagModel>> = this._tagsService.getTagsMap();
+  readonly storeList$: Observable<StoreQueryModel[]> = this.tagsMap$.pipe(
+    switchMap((tagsMap) =>
+      this._storesService.getAllStores().pipe(map((stores) => this.mapToStoreQueryModel(tagsMap, stores)))
+    )
+  );
 
-  constructor(private _categoriesService: CategoriesService) {}
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _storesService: StoresService,
+    private _tagsService: TagsService
+  ) {}
+
+  mapToStoreQueryModel(tagsMap: Record<string, TagModel>, stores: StoreModel[]): StoreQueryModel[] {
+    return stores.map((store) => ({
+      id: store.id,
+      name: store.name,
+      distanceInKm: Math.round((store.distanceInMeters * 10) / 1000) / 10,
+      logoUrl: store.logoUrl,
+      tags: (store.tagIds ?? []).map((tagId) => tagsMap[tagId].name)
+    }));
+  }
 }
