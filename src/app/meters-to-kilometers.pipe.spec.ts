9107398ad8d5c5ccc584ababd707a8ab0993c6d5
annah <anna.h.domanska@gmail.com>
2023-01-19 20:35:46 +0100

    feat: meters-to-kilometers.pipe.spec.ts removed

--- src/app/meters-to-kilometers.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MetersToKilometersPipe } from './meters-to-kilometers.pipe';
-
-describe('MetersToKilometersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MetersToKilometersPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
