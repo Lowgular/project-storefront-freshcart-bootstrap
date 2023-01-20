69b95c512aaf8c81f843c5ecdb1a19164c9b0419
annah <anna.h.domanska@gmail.com>
2023-01-20 23:36:42 +0100

    feat: category products added (with no working rating)

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, map } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { CategoryModel } from '../../models/category.model';
+import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-category-products',
@@ -17,10 +23,21 @@ export class CategoryProductsComponent {
     this._activatedRoute.params.pipe(
       switchMap((data) => this._categoriesService.getOne(data['categoryId']))
     );
-  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getAll();
+  readonly categories$: Observable<CategoryModel[]> =
+    this._categoriesService.getAll();
+
+  readonly productsInCategory$: Observable<ProductModel[]> = combineLatest([
+    this._productsService.getAll(),
+    this.categoryData$,
+  ]).pipe(
+    map(([products, category]: [ProductModel[], CategoryModel]) => {
+      return products.filter((product) => product.categoryId === category.id);
+    })
+  );
 
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _categoriesService: CategoriesService
-  ) { }
+    private _categoriesService: CategoriesService,
+    private _productsService: ProductsService
+  ) {}
 }
