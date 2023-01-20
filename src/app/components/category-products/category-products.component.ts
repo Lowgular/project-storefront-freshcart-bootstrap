b19764dd90d2be1137b1bda40dab9193373dda1e
annah <anna.h.domanska@gmail.com>
2023-01-20 22:45:27 +0100

    feat: list of categories and detail simple card added

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,11 +1,26 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { CategoryModel } from '../../models/category.model';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-category-products',
   styleUrls: ['./category-products.component.scss'],
   templateUrl: './category-products.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductsComponent {
+  readonly categoryData$: Observable<CategoryModel> =
+    this._activatedRoute.params.pipe(
+      switchMap((data) => this._categoriesService.getOne(data['categoryId']))
+    );
+  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getAll();
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _categoriesService: CategoriesService
+  ) { }
 }
