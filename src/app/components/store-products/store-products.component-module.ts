c64a80451af29062c9273c8c13e4e13e862e7a0e
annah <anna.h.domanska@gmail.com>
2023-01-20 22:00:46 +0100

    feat: detail card and searchbox added

--- src/app/components/store-products/store-products.component-module.ts
+++ src/app/components/store-products/store-products.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StoreProductsComponent } from './store-products.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [StoreProductsComponent],
   providers: [],
   exports: [StoreProductsComponent]
--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -1,11 +1,29 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { StoreModel } from '../../models/store.model';
+import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-store-products',
   styleUrls: ['./store-products.component.scss'],
   templateUrl: './store-products.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StoreProductsComponent {
+  readonly storeData$: Observable<StoreModel> =
+    this._activatedRoute.params.pipe(
+      switchMap((data) => this._storesService.getOne(data['storeId']))
+    );
+  readonly search: FormControl = new FormControl();
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _storesService: StoresService
+  ) { }
+
+ 
 }
