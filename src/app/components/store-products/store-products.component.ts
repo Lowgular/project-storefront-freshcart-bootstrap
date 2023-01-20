de480df8b727aaa1fd2c23a855647eeb827f0ce1
annah <anna.h.domanska@gmail.com>
2023-01-20 22:17:57 +0100

    feat: store produts section added

--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, map } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { StoreModel } from '../../models/store.model';
+import { ProductModel } from '../../models/product.model';
 import { StoresService } from '../../services/stores.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-store-products',
@@ -18,12 +20,21 @@ export class StoreProductsComponent {
     this._activatedRoute.params.pipe(
       switchMap((data) => this._storesService.getOne(data['storeId']))
     );
+
   readonly search: FormControl = new FormControl();
+  
+  readonly storeProducts$: Observable<ProductModel[]> = combineLatest([
+    this._productsService.getAll(),
+    this.storeData$
+  ]).pipe(
+    map(([products, storeDetails] : [ProductModel[], StoreModel]) => 
+    {return products.filter(product => product.storeIds.includes(storeDetails.id))})
+  );
 
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _storesService: StoresService
+    private _storesService: StoresService, private _productsService: ProductsService
   ) { }
 
- 
+
 }
