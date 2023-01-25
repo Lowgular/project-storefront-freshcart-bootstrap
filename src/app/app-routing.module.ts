969af66473d59903e2edb2c6054794b433853a72
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-01-25 12:43:30 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryProducuctsComponent } from './components/category-producucts/category-producucts.component';
+import { HomeComponent } from './components/home/home.component';
+import { StoreProductsComponent } from './components/store-products/store-products.component';
+import { CategoryProducuctsComponentModule } from './components/category-producucts/category-producucts.component-module';
+import { HomeComponentModule } from './components/home/home.component-module';
+import { StoreProductsComponentModule } from './components/store-products/store-products.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categories/:categoryId', component: CategoryProducuctsComponent },
+  { path: '', component: HomeComponent },
+  { path: 'stories/:storeId', component: StoreProductsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CategoryProducuctsComponentModule,
+    HomeComponentModule,
+    StoreProductsComponentModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
