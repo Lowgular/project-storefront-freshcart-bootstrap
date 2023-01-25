b080678a94392b14106bca2ff8042744e4f172ee
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-01-25 12:42:20 +0100

    add cagtegory-products component

---
+++ src/app/components/category-producucts/category-producucts.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-category-producucts',
+  styleUrls: ['./category-producucts.component.scss'],
+  templateUrl: './category-producucts.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CategoryProducuctsComponent {
+}
