d29b88db9fef1bd6a0c10bcf6780791c53a8db56
Kamkoz <kamkoz93@gmail.com>
2023-01-27 08:41:08 +0100

    feat: removed commented block of code

--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -5,16 +5,7 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import {
-  filter,
-  map,
-  shareReplay,
-  startWith,
-  switchMap,
-  take,
-  tap,
-} from 'rxjs/operators';
+import { Observable, map, shareReplay, switchMap, take, tap } from 'rxjs';
 import { StoresService } from '../../services/stores.service';
 import { ProductsService } from '../../services/products.service';
 import { ProductModel } from '../../models/product.model';
@@ -27,65 +18,6 @@ import { StoreModel } from '../../models/store.model';
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-// export class StoreProductsComponent implements OnInit {
-//   constructor(
-//     private _storesService: StoresService,
-//     private _activatedRoute: ActivatedRoute,
-//     private _productsService: ProductsService,
-//     private _router: Router
-//   ) {}
-
-//   readonly pageParams$: Observable<Params> =
-//     this._activatedRoute.queryParams.pipe(
-//       map((params) => ({
-//         storeId: params['storeId'],
-//       })),
-//       startWith({ storeId: 'defaultValue' }),
-//       shareReplay(1)
-//     );
-
-// readonly storeProducts$: Observable<ProductModel[]> = this._productsService
-//   .getAllProducts()
-//   .pipe(
-//     map((products) =>
-//       products.filter((product) => product.storeIds.includes('1'))
-//     )
-//   );
-// readonly storeProducts$: Observable<ProductModel[]> = this.pageParams$.pipe(
-//   switchMap((queryParams) => {
-//     const storeId = queryParams['storeId'];
-//     return this._productsService
-//       .getAllProducts()
-//       .pipe(
-//         map((products) =>
-//           products.filter(
-//             (product) => product.storeIds.indexOf(storeId) !== -1
-//           )
-//         )
-//       );
-//   })
-// );
-
-// readonly store$: Observable<StoreModel> = this.pageParams$.pipe(
-//   switchMap((params) => this._storesService.getStoreById(params['storeId'])),
-//   shareReplay(1)
-// );
-
-// ngOnInit() {
-//   this.pageParams$
-//     .pipe(
-//       take(1),
-//       tap((params) => {
-//         console.log(params);
-//         this._router.navigate([], {
-//           queryParams: {
-//             storeId: params['storeId'],
-//           },
-//         });
-//       })
-//     )
-//     .subscribe();
-// }
 export class StoreProductsComponent implements OnInit {
   constructor(
     private _storesService: StoresService,
