cc2fe680dfe534bed9ecd0e268623d2f0e6c141f
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-01-27 11:07:52 +0100

    store details

--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -3,6 +3,10 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
+import { StoreModel } from 'src/app/models/store.model';
+import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-store-products',
@@ -11,4 +15,13 @@ import {
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StoreProductsComponent {}
+export class StoreProductsComponent {
+  readonly store$: Observable<StoreModel> = this._activatedRoute.params.pipe(
+    switchMap((params) => this._storesService.getOneStore(params['storeId']))
+  );
+
+  constructor(
+    private _storesService: StoresService,
+    private _activatedRoute: ActivatedRoute
+  ) {}
+}
