b182086c3c855f821a028d5a36342334f0f7c2f4
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-01-29 19:38:10 +0100

    add cards of products

--- src/app/components/category-producucts/category-producucts.component.ts
+++ src/app/components/category-producucts/category-producucts.component.ts
@@ -3,11 +3,14 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute, Params } from '@angular/router';
+import { combineLatest, Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { ProductQueryModel } from 'src/app/query-models/product.query-model';
 import { CategoryModel } from '../../models/category.model';
+import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-category-producucts',
@@ -25,9 +28,50 @@ export class CategoryProducuctsComponent {
     );
   readonly categories$: Observable<CategoryModel[]> =
     this._categoriesService.getAllCategories();
+  readonly products$: Observable<ProductQueryModel[]> = combineLatest([
+    this._activatedRoute.params,
+    this._productsService.getAllProducts(),
+  ]).pipe(
+    map(([params, products]: [Params, ProductModel[]]) => {
+      return products
+        .filter((product) => product.categoryId === params['categoryId'])
+        .map((product) => ({
+          name: product.name,
+          price: product.price,
+          ratingValue: product.ratingValue,
+          ratingCount: product.ratingCount,
+          imageUrl: product.imageUrl,
+          featureValue: product.featureValue,
+          storeIds: product.storeIds,
+          id: product.id,
+          starsRating: this._showStars(product.ratingValue),
+        }));
+    })
+  );
+  readonly rating$: Observable<any> = of([
+    { value: 5, star: this._showStars(5) },
+    { value: 4, star: this._showStars(4) },
+    { value: 3, star: this._showStars(3) },
+    { value: 2, star: this._showStars(2) },
+  ]);
 
   constructor(
     private _categoriesService: CategoriesService,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
+    private _productsService: ProductsService
   ) {}
+  private _showStars(ratingValue: number): number[] {
+    let stars = [];
+    let index = 1;
+    for (index; ratingValue >= index; index++) {
+      stars.push(1);
+    }
+    if (ratingValue - stars.length >= 0.5) {
+      stars.push(0.5);
+    }
+    for (index; stars.length < 5; index++) {
+      stars.push(0);
+    }
+    return stars;
+  }
 }
