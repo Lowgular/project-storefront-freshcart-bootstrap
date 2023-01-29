f4044359f2a96e09b5af26c886dd3a74ee170631
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-01-29 17:46:45 +0100

    porducts list and basic detail

--- src/app/components/category-producucts/category-producucts.component.ts
+++ src/app/components/category-producucts/category-producucts.component.ts
@@ -1,11 +1,33 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { CategoryModel } from '../../models/category.model';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-category-producucts',
   styleUrls: ['./category-producucts.component.scss'],
   templateUrl: './category-producucts.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProducuctsComponent {
+  readonly categoryDetail$: Observable<CategoryModel> =
+    this._activatedRoute.params.pipe(
+      switchMap((params) =>
+        this._categoriesService.getOneCategory(params['categoryId'])
+      )
+    );
+  readonly categories$: Observable<CategoryModel[]> =
+    this._categoriesService.getAllCategories();
+
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _activatedRoute: ActivatedRoute
+  ) {}
 }
