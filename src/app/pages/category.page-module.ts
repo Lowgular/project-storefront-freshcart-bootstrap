b33b4c5f0d1cf52fd29ebb372e517d48066125b2
Damian Rycerz <damianrycerz10@gmail.com>
2023-03-15 15:39:15 +0100

    pages created

---
+++ src/app/pages/category.page-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CategoryPage } from './category.page';
+
+@NgModule({
+  imports: [],
+  declarations: [CategoryPage],
+  providers: [],
+  exports: [CategoryPage]
+})
+export class CategoryPageModule {
+}
---
+++ src/app/pages/store.page-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { StorePage } from './store.page';
+
+@NgModule({
+  imports: [],
+  declarations: [StorePage],
+  providers: [],
+  exports: [StorePage]
+})
+export class StorePageModule {
+}
