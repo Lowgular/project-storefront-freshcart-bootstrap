9c98978d632a93c68ab78eac3afd7341df8a1aaf
Damian Rycerz <damianrycerz10@gmail.com>
2023-03-15 16:12:28 +0100

    services and dto created

---
+++ src/app/lib/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/adapters/secondary/services/categories/categories.service-module';
+export * from './lib/adapters/secondary/services/stores/stores.service-module';
+export * from './lib/adapters/secondary/services/products/products.service-module';
---
+++ src/app/lib/src/lib/adapters/secondary/services/stores/stores.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {StoreDTO} from '../../../../application/ports/secondary/dto/store.dto';
+
+@Injectable()
+export class StoresService {
+  constructor(private _httpClient: HttpClient) {}
+
+  getAll(): Observable<StoreDTO[]> {
+    return this._httpClient.get<StoreDTO[]>(
+      `https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores`
+    );
+  }
+
+  getOne(id: string): Observable<StoreDTO[]> {
+    return this._httpClient.get<StoreDTO[]>(
+      `ttps://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores/${id}`
+    );
+  }
+}
